b47591f31205d615e5d2f9e0eed5935ba3b64d77
Big Papa <brett.cole@Rachels-MacBook-Air.local>
2023-06-06 17:08:27 -0400

    Initial commit with initial express routes and middleware error function handling

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import router from './routes/userRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+dotenv.config();
+const port = process.env.PORT || 4200;
+
+const app = express();
+
+app.get('/', (req, res) => res.send('API running'));
+
+app.listen(port, () => console.log(`Server started on port ${port}`)); 
+
+app.use('/api/users', router);
+
+app.use(notFound);
+app.use(errorHandler);
